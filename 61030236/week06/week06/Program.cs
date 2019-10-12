8ec47cbefbd26aec96016c9e5206491c003bc6b8
Toeyty <61030236@kmitl.ac.th>
2019-10-12 11:49:18 +0700

    ตัวดำเนินการกำหนดค่าแบบแบบมีเงื่อนไข (Conditional Assignment Operators

--- 61030236/week06/week06/Program.cs
+++ 61030236/week06/week06/Program.cs
@@ -10,7 +10,8 @@ namespace week06
     {
         static void Main(string[] args)
         {
-
+            double GPA = 1.95;
+            Console.WriteLine("You are " + (GPA >= 2.0 ? "pass" : "fall"));
         }
     }
 }
