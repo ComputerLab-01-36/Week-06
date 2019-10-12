cf1c2b7f818e43eec902624e7c25b4e2dada355a
Toeyty <61030236@kmitl.ac.th>
2019-10-12 11:33:53 +0700

    การสร้างเมธอดที่รับพารามิเตอร์แบบ Reference parameters

--- 61030236/week06/week06/Program.cs
+++ 61030236/week06/week06/Program.cs
@@ -10,7 +10,15 @@ namespace week06
     {
         static void Main(string[] args)
         {
+            int val = 999;
+            refMethod(ref val);
+            Console.WriteLine(val);
+            Console.ReadLine();
+        }
 
+        static void refMethod(ref int i)
+        {
+            i = i + 1;
         }
     }
 }
