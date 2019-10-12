70d7b040486797481e15eec9c568309ce2909deb
Toeyty <61030236@kmitl.ac.th>
2019-10-12 11:13:27 +0700

    ตัวอย่างการเรียกใช้งานเมธอดที่จัดเตรียมไว้แล้ว

--- 61030236/week06/week06/Program.cs
+++ 61030236/week06/week06/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;  // for web client
+using System.IO;   // for stream writer
 
 namespace week06
 {
@@ -10,7 +12,17 @@ namespace week06
     {
         static void Main(string[] args)
         {
-
+            // Create web client.
+            WebClient client = new WebClient();
+            // Download string.
+            string value = client.DownloadString("http://www.kmitl.ac.th");
+            // Write values.
+            Console.WriteLine("--- WebClient result ---");
+            Console.WriteLine(value.Length);
+            Console.WriteLine(value);
+            StreamWriter outtxt = new StreamWriter("webload.html");
+            outtext.Write(value);
+            outtxt.Dispose();
         }
     }
 }
