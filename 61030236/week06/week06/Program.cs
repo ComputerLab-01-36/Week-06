d2ca2549416773f4707aed5041e366291955d7f8
Toeyty <61030236@kmitl.ac.th>
2019-10-12 11:31:53 +0700

    การสร้างเมธอดที่รับพารามิเตอร์แบบไม่จำกัดจำนวนและชนิดด้วย Parameter arrays

--- 61030236/week06/week06/Program.cs
+++ 61030236/week06/week06/Program.cs
@@ -10,7 +10,17 @@ namespace week06
     {
         static void Main(string[] args)
         {
+            object[] myObjArray = { 2, 'b', 1000.05d, "Hello World" };
+            useParams(myObjArray);
+            useParams("IE.Tech.", "KMITL", 2017);
+            Console.ReadLine();
+        }
 
+        static void useParams(params object[] list)
+        {
+            foreach (object o in list)
+                Console.Write(o + " ");
+            Console.WriteLine();
         }
     }
 }
