a3010e9711aaa77548a5e1ebe69da2753f7238c8
Toeyty <61030236@kmitl.ac.th>
2019-10-12 11:50:52 +0700

    ตัวดำเนินการเปรียบเทียบ ให้ผลลัพธ์เป็นชนิดข้อมูล bool

--- 61030236/week06/week06/Program.cs
+++ 61030236/week06/week06/Program.cs
@@ -10,7 +10,13 @@ namespace week06
     {
         static void Main(string[] args)
         {
-
+            int x = 10, y = 5;
+            Console.WriteLine("x > y : " + (x > y));
+            Console.WriteLine("x < y : " + (x < y));
+            Console.WriteLine("x >= y : " + (x >= y));
+            Console.WriteLine("x <= y : " + (x <= y));
+            Console.WriteLine("x == y : " + (x == y));
+            Console.WriteLine("x != y : " + (x != y));
         }
     }
 }
