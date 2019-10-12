4730211055a93db622467617cb7a0b37dae4c055
Toeyty <61030236@kmitl.ac.th>
2019-10-12 11:54:45 +0700

    ตัวดำเนินการไบนารี่ (Binary operators) หรือตัวดำเนินการระดับบิต (Bitwise Operators)

--- 61030236/week06/week06/Program.cs
+++ 61030236/week06/week06/Program.cs
@@ -10,7 +10,16 @@ namespace week06
     {
         static void Main(string[] args)
         {
+            byte a = 3;
+            byte b = 5;
 
+            Console.WriteLine(a | b);
+            Console.WriteLine(a & b);
+            Console.WriteLine(a ^ b);
+            Console.WriteLine(~a & b);
+            Console.WriteLine(a << 1);
+            Console.WriteLine(a << 2);
+            Console.WriteLine(a >> 1);
         }
     }
 }
